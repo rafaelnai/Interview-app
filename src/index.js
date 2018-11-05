a6f28a6a5b584b444cda5f48689204168e4e5154
Rafael Nai <ramiresnai@gmail.com>
2018-11-05 15:20:25 -0200

    routes added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Notepad from './components/Notepad';
+import ListCards from './components/ListCards';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+  <Switch>
+    <Route path='/' exact component={ListCards}/>
+    <Route path='/interview/:id' exact component={App}/>
+    <Route path='*' component={Notepad}/>
+  </Switch>
+  </BrowserRouter>
+  , document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
