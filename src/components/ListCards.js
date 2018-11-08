04bc29ade9d1cc3846a98a3654722968b227b03c
Rafael Nai <ramiresnai@gmail.com>
2018-11-07 23:51:58 -0200

    if no cards display a msg

--- src/components/ListCards.js
+++ src/components/ListCards.js
@@ -5,23 +5,37 @@ import jsonResponse from '../jsonResponse'
 const ListCards = props => {
 
   const blockedPerson = !!props.location.params ? props.location.params.blockedPerson : []
-  // jsonResponse.forEach(person => blockedPerson.indexOf(person.idPerson) === -1 ? true : false)
+
+  const displayPerson = jsonResponse
+    .filter(person => blockedPerson.indexOf(person.idPerson) === -1)
+
+  const styleTextStrong = {
+    position: 'absolute',
+    textAlign: 'center',
+    width: '100%',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    fontSize: '2rem'
+  }
 
   return(
     <div className="row">
       {
-        jsonResponse
-        .filter(person => blockedPerson.indexOf(person.idPerson) === -1)
-        .map(response =>
-          <Card
-            key={response.idPerson}
-            imagePath={response.image}
-            content={response.description}
-            idPerson={response.idPerson}
-            params={!!props.location.params && props.location.params}
-            action={true}
-          />
-        )
+        !displayPerson.length ?
+          displayPerson.map(response =>
+            <Card
+              key={response.idPerson}
+              imagePath={response.image}
+              content={response.description}
+              idPerson={response.idPerson}
+              params={!!props.location.params && props.location.params}
+              action={true}
+            />
+          )
+          :
+          <strong className="flow-text" style={styleTextStrong}>
+            Não existem mais entrevistas!
+          </strong>
       }
     </div>
   )
